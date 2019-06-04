cb5cdf60d128b5ff754b00a189c4eb45d5b5b3e5
wyj <291685399@qq.com>
2019-06-04 08:38:10 +0800

    修改freemarker中的命名

--- springboot-freemarker01/src/main/java/com/wyj/APIApplication.java
+++ springboot-freemarker01/src/main/java/com/wyj/APIApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class APIApplication {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(APIApplication.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 
 }
